7336e80107b3bcee35603cb3c427628e7e9b5a6e
Samuliak <samuliak77@gmail.com>
2024-07-17 10:34:13 +0200

    break everything :)

--- src/pica200/builder.zig
+++ src/pica200/builder.zig
@@ -97,8 +97,8 @@ const Type = union(TypeId) {
             .Int => 1,
             .Float => 1,
             .Vector => 1,
-            .Matrix => self.Matrix.column_count * self.Matrix.column_type.getRegisterCount(),
-            .Array => self.Array.element_count * self.Array.element_type.getRegisterCount(),
+            .Matrix => |mtx_type| mtx_type.column_count * mtx_type.column_type.getRegisterCount(),
+            .Array => |arr_type| arr_type.element_count * arr_type.element_type.getRegisterCount(),
             .Struct => |struct_type| {
                 var result: u32 = 0;
                 for (struct_type.member_types) |member_type| {
@@ -118,12 +118,12 @@ const Value = struct {
     component_indices: [4]i8,
     constant: ?Constant,
 
-    pub fn init(id: u32, name: []const u8, register_index: u32, ty: *const Type) Value {
+    pub fn init(id: u32, name: []const u8, register_index: u32, ty: Type) Value {
         var self: Value = undefined;
         self.id = id;
         self.name = name;
         self.register_index = register_index;
-        self.ty = ty.*;
+        self.ty = ty;
         self.component_indices[0] = 0;
         self.component_indices[1] = 1;
         self.component_indices[2] = 2;
@@ -133,24 +133,12 @@ const Value = struct {
         return self;
     }
 
-    pub fn indexInto(self: *const Value, builder: *Builder, allocator: std.mem.Allocator, index_v: *const Value) !Value {
+    pub fn indexInto(self: *const Value, allocator: std.mem.Allocator, index_v: *const Value) !Value {
         var result = self.*;
         switch (self.ty) {
             .Array => |array_type| {
                 result.ty = array_type.element_type.*;
                 result.name = try std.fmt.allocPrint(allocator, "{s}[{}]", .{self.name, index_v.constant.?.toIndex()});
-            },
-            .Struct => |struct_type| {
-                const index = index_v.constant.?.toIndex();
-                result.id = self.id | (index << MEMBER_INDEX_SHIFT);
-                result.ty = struct_type.member_types[index].*;
-
-                const decoration_props = builder.decoration_map.get(result.id);
-                if (decoration_props) |d| {
-                    result.register_index = d.location;
-                } else {
-                    result.register_index += index;
-                }
             },
             else => {
                 result.component_indices[0] = result.component_indices[index_v.constant.?.toIndex()];
@@ -214,16 +202,17 @@ pub const Builder = struct {
 
     // Decoration instructions
     pub fn createDecoration(self: *Builder, target_id: u32, decoration: Decoration) !void {
-        try createMemberDecoration(self, target_id, 0, decoration);
-    }
-
-    pub fn createMemberDecoration(self: *Builder, target_id: u32, member_index: u32, decoration: Decoration) !void {
-        const decoration_props = try self.decoration_map.getOrPut(target_id | (member_index << MEMBER_INDEX_SHIFT));
+        const decoration_props = try self.decoration_map.getOrPut(target_id);
         switch (decoration) {
             .Location => |location| decoration_props.value_ptr.location = location,
             .Position => decoration_props.value_ptr.position = true,
             else => {},
         }
+        std.debug.print("Decorated {}: {}\n", .{target_id, decoration});
+    }
+
+    pub fn createMemberDecoration(self: *Builder, target_id: u32, member_index: u32, decoration: Decoration) !void {
+        try self.createDecoration(target_id | (member_index << MEMBER_INDEX_SHIFT), decoration);
     }
 
     // Type instructions
@@ -244,25 +233,33 @@ pub const Builder = struct {
     }
 
     pub fn createVectorType(self: *Builder, result: u32, component_type: u32, component_count: u32) !void {
-        const component_type_t = &self.type_map.get(component_type).?;
-        try self.type_map.put(result, Type{ .Vector = .{ .component_type = component_type_t, .component_count = component_count } });
+        const component_type_t = self.type_map.get(component_type);
+        if (component_type_t) |*t| {
+            try self.type_map.put(result, Type{ .Vector = .{ .component_type = t, .component_count = component_count } });
+        }
     }
 
     pub fn createMatrixType(self: *Builder, result: u32, column_type: u32, column_count: u32) !void {
-        const column_type_t = &self.type_map.get(column_type).?;
-        try self.type_map.put(result, Type{ .Matrix = .{ .column_type = column_type_t, .column_count = column_count } });
+        const column_type_t = self.type_map.get(column_type);
+        if (column_type_t) |*t| {
+            try self.type_map.put(result, Type{ .Matrix = .{ .column_type = t, .column_count = column_count } });
+        }
     }
 
     pub fn createArrayType(self: *Builder, result: u32, element_type: u32, element_count: u32) !void {
-        const element_type_t = &self.type_map.get(element_type).?;
-        try self.type_map.put(result, Type{ .Array = .{ .element_type = element_type_t, .element_count = element_count } });
+        const element_type_t = self.type_map.get(element_type);
+        if (element_type_t) |*t| {
+            try self.type_map.put(result, Type{ .Array = .{ .element_type = t, .element_count = element_count } });
+        }
     }
 
     pub fn createStructType(self: *Builder, result: u32, member_types: []const u32) !void {
         var member_types_t = std.ArrayList(*const Type).init(self.allocator.allocator());
         for (member_types) |member_type| {
-            const member_type_t = &self.type_map.get(member_type).?;
-            try member_types_t.append(member_type_t);
+            const member_type_t = self.type_map.get(member_type);
+            if (member_type_t) |*t| {
+                try member_types_t.append(t);
+            }
         }
         try self.type_map.put(result, Type{ .Struct = .{ .member_types = member_types_t.items } });
     }
@@ -271,6 +268,11 @@ pub const Builder = struct {
         const element_type_t = self.type_map.get(element_type).?;
         // Just copy the type
         try self.type_map.put(result, element_type_t);
+        // Also copy the decoration
+        if (self.decoration_map.get(element_type)) |d| {
+            try self.decoration_map.put(result, d);
+            std.debug.print("Copied decoration to {}: {}\n", .{result, d});
+        }
     }
 
     // Instructions
@@ -287,9 +289,9 @@ pub const Builder = struct {
     }
 
     pub fn createConstant(self: *Builder, writer: anytype, result: u32, ty: u32, val: u32) !void {
-        const type_v = &self.type_map.get(ty).?;
+        const type_v = self.type_map.get(ty).?;
 
-        const constant: Constant = switch (type_v.*) {
+        const constant: Constant = switch (type_v) {
             .Bool => .{ .Bool = val != 0 },
             .Int => |int| blk: {
                 if (int.is_signed) {
@@ -314,12 +316,29 @@ pub const Builder = struct {
     }
 
     pub fn createVariable(self: *Builder, writer: anytype, result: u32, ty: u32, storage_class: StorageClass) !void {
-        const type_v = &self.type_map.get(ty).?;
+        const type_v = self.type_map.get(ty).?;
 
-        const decoration_props = self.decoration_map.get(result);
-        var location: u32 = 0;
-        if (decoration_props) |d| {
-            location = d.location;
+        try self.createVariableImpl(writer, result, ty, type_v, storage_class);
+    }
+
+    pub fn createVariableImpl(self: *Builder, writer: anytype, result: u32, ty: u32, type_v: Type, storage_class: StorageClass) !void {
+        std.debug.print("Type of variable: {}\n", .{type_v});
+        std.debug.print("Result 1: {}, {}\n", .{result, ty});
+        switch (type_v) {
+            .Struct => |struct_t| {
+                for (0..struct_t.member_types.len) |i| {
+                    const member_t = struct_t.member_types[i];
+                    try self.createVariableImpl(writer, result | @as(u32, @intCast(i << MEMBER_INDEX_SHIFT)), ty | @as(u32, @intCast(i << MEMBER_INDEX_SHIFT)), member_t.*, storage_class);
+                }
+                return;
+            },
+            else => {},
+        }
+        std.debug.print("Result 2: {}\n", .{result});
+
+        var decoration_props = self.decoration_map.get(result);
+        if (decoration_props == null) {
+            decoration_props = self.decoration_map.get(ty);
         }
 
         var name: []const u8 = undefined;
@@ -327,15 +346,16 @@ pub const Builder = struct {
         switch (storage_class) {
             .Input => {
                 name = "v";
-                register_index = location;
+                register_index = decoration_props.?.location;
             },
             .Output => {
                 name = "o";
-                register_index = location;
+                std.debug.print("Type of output: {}\n", .{type_v});
+                register_index = decoration_props.?.location;
             },
             .Uniform => {
                 name = "uniform";
-                register_index = location;
+                register_index = decoration_props.?.location;
             },
             else => {
                 name = "r";
@@ -366,29 +386,38 @@ pub const Builder = struct {
         try writer.printLine("mov {s}, {s}", .{try self.getValueName(&ptr_v), try self.getValueName(&val_v)});
     }
 
-    pub fn createExtract(self: *Builder, _: anytype, result: u32, composite: u32, indices: []const u32) !void {
-        var value = self.id_map.get(composite).?;
-        for (indices) |index| {
-            var index_v = Value.init(0, "INVALID", 0, &Type{ .Int = .{ .is_signed = false } });
+    fn indexToValue(self: *Builder, index: u32, is_id: bool) *const Value {
+        if (is_id) {
+            return &self.id_map.get(index).?;
+        } else {
+            var index_v = Value.init(0, "INVALID", 0, Type{ .Int = .{ .is_signed = false } });
             index_v.constant = Constant{ .Uint = index };
-            value = try value.indexInto(self, self.allocator.allocator(), &index_v);
+
+            return &index_v;
         }
-        try self.id_map.put(result, value);
     }
 
-    pub fn createAccessChain(self: *Builder, _: anytype, result: u32, ptr: u32, indices: []const u32) !void {
+    pub fn createAccessChain(self: *Builder, writer: anytype, result: u32, ptr: u32, indices: []const u32, indices_are_ids: bool) !void {
         var value = self.id_map.get(ptr).?;
+
+        // The first index is the member index (if the pointer is a struct)
+        switch (value.ty) {
+            .Struct => {
+                const index_v = self.indexToValue(indices[0], indices_are_ids);
+                try self.createAccessChain(writer, result, ptr | (index_v.constant.?.toIndex() << MEMBER_INDEX_SHIFT), indices[1..], indices_are_ids);
+            },
+            else => {},
+        }
+
         for (indices) |index| {
-            const index_v = self.id_map.get(index);
-            if (index_v) |*ind_v| {
-                value = try value.indexInto(self, self.allocator.allocator(), ind_v);
-            }
+            const index_v = self.indexToValue(index, indices_are_ids);
+            value = try value.indexInto(self.allocator.allocator(), index_v);
         }
         try self.id_map.put(result, value);
     }
 
     pub fn createConstruct(self: *Builder, writer: anytype, result: u32, ty: u32, components: []const u32) !void {
-        const type_v = &self.type_map.get(ty).?;
+        const type_v = self.type_map.get(ty).?;
 
         const value = Value.init(result, "r", self.getAvailableRegister(type_v.getRegisterCount()), type_v);
         for (0..components.len) |i| {
@@ -399,7 +428,7 @@ pub const Builder = struct {
     }
 
     pub fn createAdd(self: *Builder, writer: anytype, result: u32, ty: u32, lhs: u32, rhs: u32) !void {
-        const type_v = &self.type_map.get(ty).?;
+        const type_v = self.type_map.get(ty).?;
 
         const lhs_v = self.id_map.get(lhs).?;
         const rhs_v = self.id_map.get(rhs).?;
@@ -410,7 +439,7 @@ pub const Builder = struct {
     }
 
     pub fn createVectorTimesScalar(self: *Builder, writer: anytype, result: u32, ty: u32, vec: u32, scalar: u32) !void {
-        const type_v = &self.type_map.get(ty).?;
+        const type_v = self.type_map.get(ty).?;
 
         const vec_v = self.id_map.get(vec).?;
         const scalar_v = self.id_map.get(scalar).?;
